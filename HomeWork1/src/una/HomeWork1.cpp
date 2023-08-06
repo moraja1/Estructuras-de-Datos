29e234f9337eb66ecc6f5fc050be40dfe6903cce
moraja1 <jeisonmora0115@gmail.com>
2023-08-06 13:45:58 -0600

    Sorting completed

--- HomeWork1/src/una/HomeWork1.cpp
+++ HomeWork1/src/una/HomeWork1.cpp
@@ -7,25 +7,69 @@
 
 using namespace std;
 
+bool equals(string const &v1, string const &v2)
+{
+    return v1 == v2;
+}
+
 list<Person> sortPersons(list<Person> const &original)
 {
     list<Person> sorted;
     
     for(Person o : original)
     {
-        if(!sorted.empty())
-        {
-            for(Person s : sorted)
-            {
-	            //Cada persona de la lista original la comparo con cada
-                //persona de la lista organizada. Así voy insertando
-                //cada persona en la lista organizada en el orden correspondiente.
-                //De momento tengo cada persona de Original como o y cada persona
-                //de sorted como s.
-            }
-        }else
+        if(sorted.empty())
         {
             sorted.push_front(o);
+            continue;
+        }
+
+        //Creo un iterador para ubicarme en cada persona de la lista.
+        list<Person>::iterator iterator;
+        bool inserted = false;
+        for(iterator = sorted.begin(); !inserted; iterator++)
+        {
+            //Variables necesarias
+            Person s = *iterator;
+            string oWord = o.getMiddleName();
+            string sWord = s.getMiddleName();
+
+            //Establezco los apellidos o el nombre a utilizar
+            if(equals(oWord, sWord))
+            {
+                oWord = o.getLastName();
+                sWord = s.getLastName();
+                if(equals(oWord, sWord))
+                {
+                    oWord = o.getName();
+                    sWord = s.getName();
+                    if(equals(oWord, sWord))
+                    {
+                        oWord = o.getId();
+                        sWord = s.getId();
+                    }
+                }
+            }
+
+            //Realizo el ordenamiento
+            for(int i = 0; i < oWord.length() && i < sWord.length(); i++)
+            {
+                if(oWord[i] != sWord[i])
+                {
+                    if (oWord[i] < sWord[i])
+                    {
+                        sorted.insert(iterator, o);
+                        inserted = true;
+                        break;
+                    }
+                    if (iterator == --sorted.end())
+                    {
+                        sorted.push_back(o);
+                        inserted = true;
+                    }
+                    break;
+                }
+            }
         }
     }
     return sorted;

161aff4be4d3283813610b38297ea736d6ecb763
Jasm <jaison.mora.viquez@gmail.com>
2023-09-23 21:00:36 -0600

    Ejercicio4 terminado

--- Examen_JaisonMoraViquez_402290912/src/main/java/cr/ac/una/ejercicio4/Main.java
+++ Examen_JaisonMoraViquez_402290912/src/main/java/cr/ac/una/ejercicio4/Main.java
@@ -1,11 +1,10 @@
 package cr.ac.una.ejercicio4;
 
 public class Main {
-    double Xn = 1;
-    double Xn1;
+    double err = 0.00001;
     public static void main(String[] args) {
         Main m = new Main();
-        System.out.println(m.raiz_cuadrada(2, 6));
+        System.out.println(m.raiz_cuadrada(2.5, 6));
     }
 
     /*
@@ -13,14 +12,20 @@ public class Main {
     Este método calcula la raíz cuadrada de un número dadas las formulas
     del examen. Saliendo del método si se cumple una de las 2 condiciones
     o itera n veces, o tiene un margen de error menor al marge propuesto
-    de 0.00001. Todo lo anterior de manera recursiva.
+    de 0.00001. Todo lo anterior de manera recursiva. Esto se puede reducir
+    a ún unico método utilizando variables globales, sin embargo, con el fin
+    de seguir las indicaciones, se hace un método adicional que reciba Xn+1 en
+    cada iteración recursiva.
 
      */
     double raiz_cuadrada(double a, int n){
+        double Xn = 1;
         double Xn1 = (Xn + (a / Xn)) / 2;
-        double err = 0.00001;
-        if ((n == 0) || (Math.abs((Xn1 - Xn)) / Xn < err)) return Xn1;
-        else Xn = Xn1;
-        return raiz_cuadrada(a, n-1);
+        return ((n == 0) || (Math.abs((Xn1 - Xn)) / Xn < err)) ? Xn1 : raiz_cuadrada(a, Xn1,n-1);
+    }
+
+    double raiz_cuadrada(double a, double Xn, int n){
+        double Xn1 = (Xn + (a / Xn)) / 2;
+        return ((n == 0) || (Math.abs((Xn1 - Xn)) / Xn < err)) ? Xn1 : raiz_cuadrada(a, Xn1,n-1);
     }
 }

dc0c1c5557636a939523e7bd80dbc2feb7775c7b
Jason Mora <jeisonmora0115@gmail.com>
2023-10-19 15:01:46 -0600

    Done Transformation Expressions Algorithm from Infix to Postfix Expressions

--- HomeWork5/src/main/java/cr/ac/una/Main.java
+++ HomeWork5/src/main/java/cr/ac/una/Main.java
@@ -11,43 +11,95 @@ public class Main {
     }
 
     private void init(String[] args) {
-        String expression = infixToPostfix("3 + (3 * (3 + 3))");
+        String expression = infixToPostfix("5 -2");
         System.out.println(expression);
     }
 
     public String infixToPostfix(String expression) {
+        StringBuilder str = new StringBuilder();
+
         expression = expression.replace(" ", "");
         expression = expression.trim();
-        Queue<String> numbers = new ArrayDeque<>();
+        Queue<String> operation = new ArrayDeque<>();
         Stack<String> operators = new Stack<>();
+        boolean priority = false;
         String slice = expression;
         if(expression.contains("(") || expression.contains(")")){
             slice = splitExpression(expression);
-            slice = infixToPostfix(slice);
+            expression = expression.substring(slice.length());
+            if(slice.contains("(") || slice.contains(")")) {
+                priority = true;
+                slice = slice.substring(1, slice.length()-1);
+                if(slice.contains("(") || slice.contains(")")) {
+                    str.append(infixToPostfix(slice));
+                }
+            }
         } else {
-            //Armo la expresion postfix
-            for(int i = 0; i < slice.length(); i++) {
-                String c = String.valueOf(slice.charAt(i));
-                if(isNumber(c)){
-                    numbers.add(c);
+            expression = expression.substring(slice.length());
+            priority = expression.isEmpty();
+        }
+        if(str.toString().isEmpty()) {
+            //Resuelvo la expresion postfix en una cola y una stack
+            while (!slice.isEmpty()) {
+                String c = obtainNextValue("", slice);
+                slice = slice.substring(c.length());
+                if (isNumber(c)) {
+                    operation.add(c);
                 } else {
+                    if ((c.equals("+") || c.equals("-")) && !operators.isEmpty()) {
+                        if (operators.peek().equals("*") || operators.peek().equals("/") || operators.peek().equals("^")) {
+                            while (!operators.isEmpty()) {
+                                operation.add(operators.pop());
+                            }
+                        }
+                    } else if (c.equals("^") && !operators.isEmpty()) {
+                        if (operators.peek().equals("*") || operators.peek().equals("/")) {
+                            while (!operators.isEmpty()) {
+                                operation.add(operators.pop());
+                            }
+                        }
+                    }
                     operators.push(c);
                 }
             }
-
-            StringBuilder str = new StringBuilder();
-            while(!numbers.isEmpty()) {
-                str.append(numbers.poll());
-            }
-            if(!slice.contains("(") && !slice.contains(")")) {
-                str.append(infixToPostfix(expression));
-            }
-            while(operators.isEmpty()) {
-                str.append(operators.pop());
-            }
-            return str.toString();
         }
-        return null;
+
+        while(!operation.isEmpty()) {
+            str.append(operation.poll()).append(" ");
+        }
+        if (!priority) {
+            str.append(infixToPostfix(expression));
+        }
+        while(!operators.isEmpty()) {
+            str.append(operators.pop()).append(" ");
+        }
+        if(priority && !expression.isEmpty()) {
+            str.append(infixToPostfix(expression));
+        }
+        return str.toString();
+    }
+
+    private String obtainNextValue(String value, String slice) {
+        if(!slice.isEmpty()) {
+            String c = String.valueOf(slice.charAt(0));
+            if(isNumber(c) || c.equals(".")) {
+                if (value.isEmpty() || isNumber(value)) {
+                    value = value.concat(c);
+                    value = obtainNextValue(value, slice.substring(1));
+                } else {
+                    return value;
+                }
+            }
+            else {
+                if (value.isEmpty()) {
+                    value = value.concat(c);
+                    value = obtainNextValue(value, slice.substring(1));
+                } else {
+                    return value;
+                }
+            }
+        }
+        return value;
     }
 
     private String splitExpression(String expression) {
@@ -67,10 +119,7 @@ public class Main {
                 closeParenthesis++;
                 if(closeParenthesis == openParenthesis) {
                     slice = expression.substring(openParenthesisIdx, i+1);
-                    if(slice.equals(expression)) {
-                        slice = slice.substring(1,slice.length()-1);
-                        break;
-                    }
+                    break;
                 }
             }
         }
--- HomeWork5/src/test/java/cr/ac/una/MainTest.java
+++ HomeWork5/src/test/java/cr/ac/una/MainTest.java
@@ -5,12 +5,61 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
+    Main m = new Main();
     @Test
     void main() {
     }
 
     @Test
     void toPostfix() {
+        String
+        expression = m.infixToPostfix("5 -2");
+        System.out.println(expression);
+        expression = m.infixToPostfix("24 + 3.8 * 5");
+        System.out.println(expression);
+        expression = m.infixToPostfix("3 + 5 + 6");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(3 + 5) + 6");
+        System.out.println(expression);
+        expression = m.infixToPostfix("3 + (5 + 6)");
+        System.out.println(expression);
+        expression = m.infixToPostfix("2^3");
+        System.out.println(expression);
+        expression = m.infixToPostfix("((8 + 9) - 5 * (4 / 5)) + 10");
+        System.out.println(expression);
+        expression = m.infixToPostfix("10 + ((8 + 9 ) - 5 * (4 / 25))");
+        System.out.println(expression);
+        expression = m.infixToPostfix("8 + 9");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(8 + 9)");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(4 / 3)");
+        System.out.println(expression);
+        expression = m.infixToPostfix("5 * (4 / 5)");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(8 + 9 ) - 5 * (4 / 7)");
+        System.out.println(expression);
+        expression = m.infixToPostfix("((8 + 9 ) - 5 * (4 / 3))");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(1 + 1)");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(((5)))");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(1 + (2 + (3 + 4)))");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(1) + 2");
+        System.out.println(expression);
+        expression = m.infixToPostfix("((((1)))) + 2");
+        System.out.println(expression);
+        expression = m.infixToPostfix("((2 * 3 + 4 * 5)) + 1.4");
+        System.out.println(expression);
+        expression = m.infixToPostfix("(2 * 3 + 4 * 5) + 1");
+        System.out.println(expression);
+        expression = m.infixToPostfix("1 * 2 + 3");
+        System.out.println(expression);
+        expression = m.infixToPostfix("1 + 2 * 3");
+        System.out.println(expression);
+        expression = m.infixToPostfix("3 + 3 * 3 + 3");
+        System.out.println(expression);
     }
 }
